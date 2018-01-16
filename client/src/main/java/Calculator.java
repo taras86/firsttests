89c43859a1d29332675eaf91bf399163e0afa690
taras86 <Pe4eneGs4>
2018-01-16 13:26:37 +0300

    Editorial changes

--- client/src/main/java/Calculator.java
+++ client/src/main/java/Calculator.java
@@ -5,4 +5,10 @@ public class Calculator {
     public int div(int a, int b){
         return a / b;
     }
+    public double sqrt(int a){
+        return Math.sqrt(a);
+    }
+    public double pow(int a, int b){
+        return Math.pow(a,b);
+    }
 }
--- tests/src/test/java/CalculatorTest.java
+++ tests/src/test/java/CalculatorTest.java
@@ -1,6 +1,8 @@
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class CalculatorTest {
 
@@ -13,6 +15,7 @@ public class CalculatorTest {
 
         assertEquals(result, calculator.sum(a, b));
     }
+
     @Test
     public void calcTest2() {
         Calculator calculator2 = new Calculator();
@@ -21,6 +24,25 @@ public class CalculatorTest {
         int result = 11;
 
         assertEquals(result, calculator2.div(c,d));
+    }
+
+    @Test
+    public void calcTest3() {
+        Calculator calculator3 = new Calculator();
+        int e = 81;
+        int result = 10;
+
+        assertThat(calculator3.sqrt(e)).as("Извлечение корня работает неправильно!").isEqualTo(result);
+    }
+
+    @Test
+    public void calcTest4() {
+        Calculator calculator4 = new Calculator();
+        int a = 2;
+        int b = 3;
+        double result = 8;
+
+        MatcherAssert.assertThat("Возведение в степень работает неправильно!", calculator4.pow(a, b), equalTo(result));
     }
 
 }
